27d0b33c6e9adf9c2a7b69be09085e561a182f1a
Arief Budiman <mariefbudiman12@gmail.com>
2023-10-20 09:27:17 +0700

    edited

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
-import MovieCard from "@/components/movieCard";
-import Link from "next/link";
+import MovieCard from '@/components/movieCard';
+import Link from 'next/link';
 
 const Home = () => {
     return (
@@ -7,8 +7,10 @@ const Home = () => {
             <h1 className="font-bold">SELAMAT DATANG DI WEBSITE MOVIE</h1>
             <MovieCard />
             <h1 className="font-bold">KENAL LEBIH JAUH DENGAN AUTHOR</h1>
-            <Link href='/profile'>
-                <button className="bg-blue-500 p-2 rounded-lg hover:bg-blue-700 text-white">Profile Author</button>
+            <Link href="/profile">
+                <button className="bg-blue-500 p-2 rounded-lg hover:bg-blue-700 text-white">
+                    Profile Author
+                </button>
             </Link>
         </div>
     );
